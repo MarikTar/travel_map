05e80306b91f90bba745c1e2116db70332885531
MarikTar <100trk@gmail.com>
2019-07-25 20:14:29 +0300

    add hover country

--- src/map.jsx
+++ src/map.jsx
@@ -20,11 +20,27 @@ let myIcon = L.icon({
 */
 
 export default class Map extends React.Component {
+    geojson = L.geoJSON(countriesJSON, {
+        style: this.styleJson.bind(this),
+        onEachFeature: this.onEachFeature.bind(this)
+    });
+    tileLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+        minZoom: 2,
+        maxZoom: 5,
+    });
+    /*
+    icon = L.divIcon({
+        html: `<button>TEXT</button>`,
+        className: 'icon123123',
+    })
+    testAddIco = L.marker([50.505, 30.57], {icon: this.icon})
+    */
     styleJson(feature) {
         return {
             color: '#000',
-            weight: 2,
-            fillOpacity: this.Opacity(feature.properties.name) || 0.8
+            weight: 1.5,
+            fillOpacity: this.Opacity(feature.properties.name) || 0.8,
+            className: feature.properties.name
         }
     }
 
@@ -35,9 +51,34 @@ export default class Map extends React.Component {
             }
         }
     }
-    onClick(e){
-        let country = e.layer.feature.properties.name;
-        this.props.setMainState(country)
+    tileLayer() {
+        return L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}',
+            {
+                minZoom: 2,
+                maxZoom: 5,
+            }
+        )
+    }
+
+    highlightFeature(e) {
+        var layer = e.target;
+        layer.setStyle({
+            weight: 3,
+            color: '#666',
+            fillOpacity: 0.7,
+        })
+    }
+    resetHighlight(e) {
+        this.geojson.resetStyle(e.target)
+    }
+
+    onEachFeature(feature, layer) {
+        const country = feature.properties.name;
+        layer.on({
+            'mouseover': this.highlightFeature,
+            'mouseout': this.resetHighlight.bind(this),
+            'click': () => this.props.setMainState(country)
+        });
     }
 
     componentDidMount() {
@@ -45,19 +86,11 @@ export default class Map extends React.Component {
             center: [55, 10],
             zoom: 2,
             zoomControl: true,
-            maxBounds: [[90, -180],[-70, 180]],
+            maxBounds: [[90, -180], [-70, 180]],
         });
-
-        L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
-            minZoom: 2,
-            maxZoom: 5,
-        }).addTo(this.map)
-
-        this.geoJson = L.geoJSON(countriesJSON, {
-            style: this.styleJson.bind(this),
-        }).addTo(this.map)
-
-        this.geoJson.on('click', (e)=>this.onClick(e))
+        this.tileLayer.addTo(this.map)
+        this.geojson.addTo(this.map);
+        //this.testAddIco.addTo(this.map)
     }
     render() {
         return (
