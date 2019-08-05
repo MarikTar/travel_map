e0a006b57a3dbb04e96a6ee503c10e5cae2260f9
Kharya1337 <ikharlamov21@gmail.com>
2019-08-05 13:49:45 +0300

    PWA commit

---
+++ build/precache-manifest.ae70aed63eb6a069abac66f088403704.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "526601413d39e04a08a1133a9331af08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "614c56c620a5e1314793",
+    "url": "/static/css/main.9991632b.chunk.css"
+  },
+  {
+    "revision": "2bf64304d36395093fec",
+    "url": "/static/js/2.f4ad80fd.chunk.js"
+  },
+  {
+    "revision": "614c56c620a5e1314793",
+    "url": "/static/js/main.5f68a7c9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b93446cf9abb836dbe76e3d0b5e0e65",
+    "url": "/static/media/checked.4b93446c.svg"
+  },
+  {
+    "revision": "043f9099f1d654de80f0f3413546e2ef",
+    "url": "/static/media/eye.043f9099.svg"
+  },
+  {
+    "revision": "58572c32382afa0fa7f7350cc6107cc9",
+    "url": "/static/media/fb.58572c32.svg"
+  },
+  {
+    "revision": "8a3f76c1c4c7af08cbe8a5011d18f555",
+    "url": "/static/media/folder-upload.8a3f76c1.svg"
+  },
+  {
+    "revision": "09aea0f59807f6f4f66af7f5719cba9e",
+    "url": "/static/media/g-pl.09aea0f5.svg"
+  },
+  {
+    "revision": "22043e8c7084d85807c5d97cfe604d27",
+    "url": "/static/media/loader.22043e8c.gif"
+  },
+  {
+    "revision": "c06c9419b44ddd5603d5fa935a2664a6",
+    "url": "/static/media/main-bg.c06c9419.jpg"
+  },
+  {
+    "revision": "5e5b511de79c6a2e4fb1b3642bdbf9b5",
+    "url": "/static/media/plus.5e5b511d.svg"
+  },
+  {
+    "revision": "4dee039c48b9926e35276cc6885a6354",
+    "url": "/static/media/search.4dee039c.svg"
+  }
+]);
