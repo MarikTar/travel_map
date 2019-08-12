fc37d5d84c0752709831ef72b20d9cd9587f4955
Kharya1337 <ikharlamov21@gmail.com>
2019-08-12 22:47:11 +0300

    Fixed index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './component/App/App';
 
-// import './index.css';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 render(<App />, document.getElementById('root'));
